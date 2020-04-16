bfde164f521292705e102c6930cbd05b351eb405
Pedro Renan <pedrorenan@gmail.com>
2020-04-15 21:11:27 -0300

    Fix to test pass

--- src/app.js
+++ src/app.js
@@ -45,7 +45,8 @@ app.put("/repositories/:id", (request, response) => {
     id,
     title,
     url,
-    techs
+    techs,
+    likes: repositories[repositoryIndex].likes
   }
 
   repositories[repositoryIndex] = repository
